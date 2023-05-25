7100ff5ef0c5a26e9371baaa577d2e071a1415da
DOEKYONG <apochmn@naver.com>
2023-05-25 18:19:12 +0900

    feat :  글 삭제
    - BlogService 글 삭제 메서드 추가
    - BlogApiController 글 삭제 매핑추가
    - 컨트롤러 테스트 코드 작성

--- src/main/java/me/light/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/light/springbootdeveloper/controller/BlogApiController.java
@@ -42,4 +42,11 @@ public class BlogApiController {
                 .body(new ArticleResponse(article));
     }
 
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+        return ResponseEntity.ok()
+                .build();
+    }
+
 }
