be0335dc680cdfd9ced5d78ce6c8a823b907f861
DOEKYONG <apochmn@naver.com>
2023-05-31 00:35:18 +0900

    feat : 글 개별 조회

--- src/main/java/me/light/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/light/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.light.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.light.springbootdeveloper.domain.Article;
 import me.light.springbootdeveloper.dto.ArticleListViewResponse;
+import me.light.springbootdeveloper.dto.ArticleViewResponse;
 import me.light.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +25,12 @@ public class BlogViewController {
                 .toList();
         model.addAttribute("articles",articles);
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article",new ArticleViewResponse(article));
+        return "article";
     }
 }
