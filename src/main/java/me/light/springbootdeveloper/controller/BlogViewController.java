500e381bfb72d24f0f9d5539fb776e5fccc966ef
DOEKYONG <apochmn@naver.com>
2023-05-30 16:47:34 +0900

    feat : 글 목록 화면 구현

---
+++ src/main/java/me/light/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package me.light.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.light.springbootdeveloper.dto.ArticleListViewResponse;
+import me.light.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles",articles);
+        return "articleList";
+    }
+}
