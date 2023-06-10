735d4d08c2755ecccac83c5bf51ec193a49489dd
DOEKYONG <apochmn@naver.com>
2023-06-10 19:31:01 +0900

    9장 jwt

---
+++ src/main/java/me/light/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package me.light.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.light.springbootdeveloper.domain.RefreshToken;
+import me.light.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
